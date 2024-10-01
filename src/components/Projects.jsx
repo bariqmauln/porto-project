b112ed35996a14686412268ecd66275a24b510f7
FreezeFOXX <bariqmaulana14@gmail.com>
2024-10-01 23:12:30 +0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/components/Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+
 import { PROJECTS } from '../constants'
 const Projects = () => {
   return (
-    <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Project</h2>
+    <div className="border-b border-neutral-900 pb-40">
+      <h2 className="my-20 text-center text-4xl">Project</h2>
       <div>
-        {PROJECTS.map((projects, index) => (
+        {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:1/4">
-              <p className="mb-2 text-sm text-neutral-400">
-                {projects.title}
-              </p>
+            <div className="w-full lg:w-1/4">
+              <img src={project.image} alt={project.title} width={150} height={150} className="mb-6 rounded"/>
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h6 className="mb-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-pink-200">
+                  {tech}
+                </span>
+              ))} 
             </div>
           </div>
         ))}
